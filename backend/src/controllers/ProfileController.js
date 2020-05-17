a6807c52a6c2c2dabec67027c0a6acdc64f48c96
sayonara <sayonaracirilo10@hotmail.com>
2020-05-16 21:46:33 -0300

    Login

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -7,7 +7,6 @@ module.exports = {
     const incidents = await connection("incidents")
       .where("ong_id", ong_id)
       .select("*");
-    console.log(incidents);
     return response.json(incidents);
   },
 };
