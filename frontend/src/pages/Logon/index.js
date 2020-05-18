7db4dc712c74f17b5b0612c48836671aef26e9d4
sayonara <sayonaracirilo10@hotmail.com>
2020-05-17 21:37:48 -0300

    front completo

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
 
 import './styles.css'
@@ -7,24 +7,40 @@ import './styles.css'
 import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 
-export default function Logon () {
+import api from '../../services/api'
+
+export default function Logon() {
+  const [id, setId] = useState('')
+  const history = useHistory()
+  async function handleLogin(e) {
+    e.preventDefault()
+    try {
+      const response = await api.post('sessions', { id })
+
+      localStorage.setItem('ongId', id)
+      localStorage.setItem('ongName', response.data.name)
+      history.push('/profile')
+    } catch (err) {
+      alert('Tente novamente')
+    }
+  }
   return (
-    <div className='logon-container'>
-      <section className='form'>
-        <img src={logoImg} alt='Be the Hero' />
-        <form>
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be the Hero" />
+        <form onSubmit={handleLogin}>
           <h1> Faça seu logon</h1>
-          <input placeholder='Sua ID'></input>
-          <button className='button' type='submit'>
+          <input placeholder="Sua ID" value={id} onChange={(e) => setId(e.target.value)}></input>
+          <button className="button" type="submit">
             Entrar
           </button>
-          <Link className='back-link' to='/register'>
-            <FiLogIn size={16} color='#E02041' />
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#E02041" />
             Não tenho Cadastro
           </Link>
         </form>
       </section>
-      <img src={heroesImg} alt='Heroes' />
+      <img src={heroesImg} alt="Heroes" />
     </div>
   )
 }
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,33 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 
 import './styles.css'
 
+import api from '../../services/api'
 import logoImg from '../../assets/logo.svg'
 
-export default function Register () {
+export default function Register() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [whatsapp, setwhatsapp] = useState('')
+  const [city, setCity] = useState('')
+  const [uf, setUf] = useState('')
+
+  const history = useHistory()
+
+  async function handleRegister(e) {
+    e.preventDefault()
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    }
+
+    try {
+      const response = await api.post('ongs', data)
+      alert(`Seu ID de acesso: ${response.data.id}`)
+      history.push('/')
+    } catch (err) {
+      alert('Erro no cadastro, tente novamente')
+    }
+  }
   return (
-    <div className='register-container'>
-      <div className='content'>
+    <div className="register-container">
+      <div className="content">
         <section>
-          <img src={logoImg} alt='Be the hero'></img>
+          <img src={logoImg} alt="Be the hero"></img>
           <h1>Cadastro</h1>
           <p>Faça seu cadastro, entre na plataforma e ajude a encontrarem os casos da sua ONG.</p>
-          <Link className='back-link' to='/'>
-            <FiArrowLeft size={16} color='#E02041' />
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#E02041" />
             Não tenho Cadastro
           </Link>
         </section>
-        <form>
-          <input placeholder='Nome da ONG' />
-          <input type='email' placeholder='E-mail' />
-          <input placeholder='WhatsApp' />
-          <div className='input-group'>
-            <input placeholder='Cidade' />
-            <input placeholder='UF' style={{ width: 80 }} />
+        <form onSubmit={handleRegister}>
+          <input placeholder="Nome da ONG" value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input placeholder="WhatsApp" value={whatsapp} onChange={(e) => setwhatsapp(e.target.value)} />
+          <div className="input-group">
+            <input placeholder="Cidade" value={city} onChange={(e) => setCity(e.target.value)} />
+            <input placeholder="UF" style={{ width: 80 }} value={uf} onChange={(e) => setUf(e.target.value)} />
           </div>
-          <button className='button' type='submit'>
+          <button className="button" type="submit">
             Cadastrar
           </button>
         </form>
